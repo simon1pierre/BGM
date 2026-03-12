1721dd64e267d284c16ea443548978055675acdd
simon1pierre <simonpierreturamyumukiza@gmail.com>
2026-03-13 00:12:43 +0200

    fixes

--- database/seeders/DatabaseSnapshotSeeder.php
+++ database/seeders/DatabaseSnapshotSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSnapshotSeeder extends Seeder
 {
